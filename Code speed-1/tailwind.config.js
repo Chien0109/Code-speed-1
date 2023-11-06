f67c229a2815f4e4ea73d28edabe8ca1bc382d74
Chien0109 <ChienVanNguyen01092002@gmail.com>
2023-11-06 14:43:32 +0700

    init

---
+++ Code speed-1/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+
+  content: ["./src/**/*.{html,js}"],
+
+  theme: {
+    
+  },
+  plugins: [],
+}
+
